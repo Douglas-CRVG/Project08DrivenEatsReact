354db4708ddab146d371fe26575fdf09f9174f87
Douglas-CRVG <douglas.07.nascimento@gmail.com>
2021-08-28 13:26:53 -0300

    selected with deprecated style

--- src/components/others/Product.js
+++ src/components/others/Product.js
@@ -1,9 +1,23 @@
+import React, {useState} from 'react'
+
 export default function Product(props) {
-    console.log(props);
     const {product} = props;
     const {imgProduct, nameProduct, descriptionProduct, priceProduct } = product;
+    const [select, setSelect] = useState("");
+    const [coisa, setCoisa] = useState(false);
+
+    function selecionar() {
+        setCoisa(() => !coisa);
+
+        if (select === "") {
+            setSelect("select");
+        } else {
+            setSelect("");
+        }
+    }
+
     return (
-        <div className="itens margem-esq sobremesa">
+        <div className={`itens margem-esq ${select}`} onClick = {selecionar} >
             <img src={imgProduct} alt="" />
             <p className="produto">{nameProduct}</p>
             <p className="descricao">{descriptionProduct}</p>
