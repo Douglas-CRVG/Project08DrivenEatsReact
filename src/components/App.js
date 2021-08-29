11a35af58b9199325ebea0e4303f25a435728ffc
Douglas-CRVG <douglas.07.nascimento@gmail.com>
2021-08-29 15:52:22 -0300

    selection of buggy items that need correction

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import Header from './header/Header';
 import Main from './main/Main';
 import Footer from './footer/Footer';
+import "./css/styles.css";
+import "./css/reset.css";
 
 export default function App() {
     return (
--- src/components/others/Product.js
+++ src/components/others/Product.js
@@ -4,29 +4,45 @@ export default function Product(props) {
     const { product } = props;
     const { imgProduct, nameProduct, descriptionProduct, priceProduct } = product;
     const [select, setSelect] = useState("");
-    const [coisa, setCoisa] = useState(false);
+    const [hidden, setHidden] = useState("desabilitado");
+    const [counter, setCounter] = useState(1);
 
     function selecionar() {
-        setCoisa(() => !coisa);
-
         if (select === "") {
             setSelect("select");
+            setHidden("")
         } else {
             setSelect("");
+            setHidden("desabilitado")
+        }
+    }
+
+    function quantity(what) {
+        if (what === "add") {
+            setCounter(counter + 1);
+        } else {
+            setCounter(counter - 1);
+        }
+
+        if (counter < 2) {
+            setSelect("");
+            setHidden("desabilitado");
         }
     }
 
     return (
-        <div className={`item margem-esq ${select}`} onClick={selecionar}>
-            <div class="product">
+        <div className={`item margem-esq ${select}`} >
+            <div class="product" onClick={selecionar}>
                 <img src={imgProduct} alt="" />
                 <p className="name">{nameProduct}</p>
                 <p className="description">{descriptionProduct}</p>
-                <p className="price">R$ <strong>{priceProduct}</strong>
-                </p>
+                <p className="price">R$ <strong>{priceProduct}</strong></p>
+            </div>
+            <div class={`container-counter ${hidden}`}>
+                <ion-icon onClick={() => quantity("sub")} name="remove-outline"></ion-icon>
+                {counter}
+                <ion-icon onClick={() => quantity("add")} name="add-outline"></ion-icon>
             </div>
         </div>
     );
 }
-
-
