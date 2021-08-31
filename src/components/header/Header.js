617620fff7add5161f05baf4c1a51407aee57e72
ligia-davila-bozzi <ligia@respondeai.com.br>
2021-08-31 14:21:16 -0300

    preparando review

--- src/components/header/Header.js
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <header className="topo">
-            <p className="logo margem-esq">FoodCamp</p>
-            <p className="sub-logo margem-esq">Sua comida em 6 minutos</p>
-        </header>
-    );
-}
